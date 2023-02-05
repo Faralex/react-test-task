55b6d80009ea8e4016ba808855924e94832453e1
Faralex <farakrikqq@mail.ru>
2023-02-05 04:34:59 +0300

    deployment

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Home from "./components/Home";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename="/react-test">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
